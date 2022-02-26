44d3d8bdb9d2f995f369a0e70daa9bc8b8d368bd
alexkcode <alexiskwan.uiuc@gmail.com>
2022-02-25 18:04:18 -0600

    fixed some queue binding logic

--- server/app.py
+++ server/app.py
@@ -19,7 +19,7 @@ def webhook_endpoint(queue):
 
     request_json = request.get_json()
     request_json.update({'queue': queue})
-    data = json.dumps(request_json,indent=4)
+    data = json.dumps(request_json)
 
     credentials = pika.PlainCredentials(request.authorization.username, request.authorization.password)
     connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq', 5672, '/', credentials))
@@ -27,18 +27,18 @@ def webhook_endpoint(queue):
     channel = connection.channel()
     channel.exchange_declare(exchange='webhook_exchange', durable=True)
     channel.queue_declare(queue=queue, durable=True)
-    channel.queue_bind(queue, 'webhook_exchange', routing_key='webhook')
+    channel.queue_bind(queue, 'webhook_exchange', routing_key=queue+"_key")
     channel.basic_publish(
         exchange='webhook_exchange',
-        routing_key='webhook',
+        routing_key=queue+"_key",
         body=data,
         properties=pika.BasicProperties(
             delivery_mode=2,  # make message persistent
         ))
     connection.close()
 
-    # return " [x] Data put in queue: %s" % data
-    return Response(response='OK', status=200)
+    return " [x] Data put in queue: %s" % data
+    # return Response(response='OK', status=200)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
