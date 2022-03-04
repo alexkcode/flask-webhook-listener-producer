5f309d03ba6fd51a10d627eed323ec102938a52a
alexkcode <alexiskwan.uiuc@gmail.com>
2022-03-04 02:26:18 -0600

    dirty fix

--- server/app.py
+++ server/app.py
@@ -1,15 +1,23 @@
-import json
+import json, ssl
 import pika
-from flask import Flask, request, abort, Response, logging
+from flask import Flask, request, redirect, Response, logging
+from flask_talisman import Talisman
 
 app = Flask(__name__)
 
 LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) '
               '-35s %(lineno) -5d: %(message)s')
 
+# @app.before_request
+# def before_request():
+#     url = request.url.replace('https://', 'http://', 1)
+#     code = 301
+#     return redirect(url)
+
 @app.route('/')
 def index():
     return 'OK'
+    # return(ssl.OPENSSL_VERSION)
 
 @app.route('/webhook/<queue>', methods=['POST'])
 def webhook_endpoint(queue):
@@ -47,4 +55,7 @@ def webhook_endpoint(queue):
     return Response(response='OK', status=200)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    # ctx = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+    # ctx.load_cert_chain('sertnew/se-emulator.crt', 'sertnew/se-emulator.key')
+    # app.run(debug=True, host='0.0.0.0', ssl_context=ctx)
+    app.run(debug=True, host='0.0.0.0', ssl_context='adhoc')
